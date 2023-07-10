10cd1855a76f680445238059454065b7c156819d
Постернак Евгений Валерьевич <EVaPosternak@sberbank.ru>
2023-07-10 17:54:34 +0300

    add swagger for auth

--- internal/auth/internal/server/server.go
+++ internal/auth/internal/server/server.go
@@ -4,9 +4,13 @@ import (
 	"context"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/kripsy/gophermart/internal/auth/internal/handler"
 	"github.com/kripsy/gophermart/internal/auth/internal/logger"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"go.uber.org/zap"
+
+	_ "github.com/kripsy/gophermart/docs/auth"
 )
 
 type Server struct {
@@ -24,8 +28,20 @@ func InitServer(ctx context.Context) (*Server, error) {
 		return nil, err
 	}
 
-	m.Router.HandleFunc("/test", h.TestHandler)
+	m.Router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
 	m.Router.Post("/api/register", h.RegisterUserHandler)
+
+	m.Router.Get("/swagger/*", httpSwagger.WrapHandler)
 
 	return m, nil
 }
