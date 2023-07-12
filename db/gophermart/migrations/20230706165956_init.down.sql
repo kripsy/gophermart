f23080be3511ac5bc55b66858f2210ffbf8ab618
Pavel Korsakov <PKorsakov@datafort.ru>
2023-07-12 22:28:04 +0700

    issue-15 Add migration for gophermart in postgres

--- db/gophermart/migrations/20230706165956_init.down.sql
+++ db/gophermart/migrations/20230706165956_init.down.sql
@@ -1,8 +1,8 @@
 begin transaction;
 
-drop index if exists public.gophermart_order_user_id_index_hash;
+drop index if exists public.gophermart_order_username_index_hash;
 drop index if exists public.gophermart_order_status_index_hash;
-drop index if exists public.gophermart_balance_balance_user_id_index_hash;
+drop index if exists public.gophermart_balance_balance_username_index_hash;
 
 drop table if exists public.gophermart_order;
 drop table if exists public.gophermart_balance;
--- db/gophermart/migrations/20230706165956_init.up.sql
+++ db/gophermart/migrations/20230706165956_init.up.sql
@@ -3,7 +3,7 @@ begin transaction;
 create table if not exists public.gophermart_order
 (
     id           bigint primary key       not null,
-    user_id      text                     not null,
+    username     varchar(255)             not null,
     number       text                     not null unique,
     status       text,
     accrual      decimal,
@@ -11,19 +11,19 @@ create table if not exists public.gophermart_order
     processed_at timestamp with time zone
 );
 
-create index gophermart_order_user_id_index_hash on public.gophermart_order using hash (user_id);
+create index gophermart_order_username_index_hash on public.gophermart_order using hash (username);
 create index gophermart_order_status_index_hash on public.gophermart_order using hash (status);
 
 create table if not exists public.gophermart_balance
 (
     id           bigint primary key,
-    user_id      text                     not null,
+    username     varchar(255)             not null,
     current      decimal,
     withdrawn    decimal,
     uploaded_at  timestamp with time zone not null default now(),
     processed_at timestamp with time zone
 );
 
-create index gophermart_balance_balance_user_id_index_hash on public.gophermart_balance using hash (user_id);
+create index gophermart_balance_balance_username_index_hash on public.gophermart_balance using hash (username);
 
 commit;
