085e527cb67ad96f3d4b65598b2c9d769ca9f260
Pavel Korsakov <PKorsakov@datafort.ru>
2023-07-14 13:12:52 +0700

    issue-35

---
+++ internal/accrual/internal/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"flag"
+	"os"
+)
+
+type Config struct {
+	RunAddress      string
+	DatabaseAddress string
+	LoggerLevel     string
+}
+
+var cfg = &Config{}
+
+func GetConfig() *Config {
+	return cfg
+}
+
+func InitConfig() *Config {
+	runAddress := flag.String(
+		"a",
+		os.Getenv("RUN_ADDRESS"),
+		"Enter bind address for serve auth server as ip_address:port. Or use RUN_ADDRESS env")
+
+	databaseAddress := flag.String(
+		"d",
+		os.Getenv("DATABASE_URI"),
+		"Enter address exec http server as postgres://username:password@hostname:portNumber/databaseName?sslmode=disable. Or use DATABASE_URI env")
+
+	loggerLevel := flag.String(
+		"l",
+		os.Getenv("LOGGER_LEVEL"),
+		"Enter logger level as Warn. Or use LOGGER_LEVEL env")
+
+	flag.Parse()
+
+	if *runAddress == "" {
+		*runAddress = "localhost:8080"
+	}
+
+	if *databaseAddress == "" {
+		*databaseAddress = "postgres://gophermart:RASKkCt3PVEU@localhost:5432/auth?sslmode=disable"
+	}
+
+	if *loggerLevel == "" {
+		*loggerLevel = "Warn"
+	}
+
+	cfg = &Config{
+		RunAddress:      *runAddress,
+		DatabaseAddress: *databaseAddress,
+		LoggerLevel:     *loggerLevel,
+	}
+
+	return cfg
+}
---
+++ internal/accrual/internal/handler/handler.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/kripsy/gophermart/internal/accrual/internal/logger"
+)
+
+type Handler struct {
+	ctx context.Context
+}
+
+func InitHandler(ctx context.Context) (*Handler, error) {
+	h := &Handler{
+		ctx: ctx,
+	}
+	return h, nil
+}
+
+func (h *Handler) CreateOrderHandler(rw http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *Handler) ReadOrdersHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *Handler) ReadUserBalanceHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *Handler) CreateWithdrawHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *Handler) ReadWithdrawsTestHandler(w http.ResponseWriter, r *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *Handler) TestHandler(w http.ResponseWriter, r *http.Request) {
+	l := logger.LoggerFromContext(h.ctx)
+	l.Debug("TestHandler")
+	w.Header().Add("Content-Type", "plain/text")
+	w.Write([]byte("Hello world"))
+}
